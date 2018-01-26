76d22d048212dcd633b0ca48b9d597391d760c21
ZhangMingZhao1 <lawliet_zmz@foxmail.com>
2018-01-27 03:46:54 +0800

    打包测试

---
+++ src/app.vue
@@ -0,0 +1,21 @@
+<!--  -->
+<template>
+    <div id='test'>{{test}}</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                test:'abc'
+            }
+        }
+
+    }
+</script>
+
+<style>
+#test {
+    color: red;
+}
+</style>
